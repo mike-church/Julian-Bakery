4e250814f4e5e68adc11d76630b10e1aeca05964
Michael Church <michael@fishinglounge.com>
2015-09-16 10:14:00 -0400

    progress dev

--- wp-content/themes/sage/functions.php
+++ wp-content/themes/sage/functions.php
@@ -29,6 +29,7 @@ foreach ($sage_includes as $file) {
 }
 unset($file, $filepath);
 
+
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
@@ -46,3 +47,4 @@ function my_theme_wrapper_end() {
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
+}
