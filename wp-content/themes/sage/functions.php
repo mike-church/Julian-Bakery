fa397ec0773a72170655440b8738a2b432221255
Michael Church <michael@fishinglounge.com>
2015-09-16 09:40:49 -0400

    progress dev

--- wp-content/themes/sage/functions.php
+++ wp-content/themes/sage/functions.php
@@ -29,9 +29,6 @@ foreach ($sage_includes as $file) {
 }
 unset($file, $filepath);
 
-
-
-
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
@@ -49,4 +46,3 @@ function my_theme_wrapper_end() {
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
-}
--- wp-content/themes/sage/templates/content-page.php
+++ wp-content/themes/sage/templates/content-page.php
@@ -1,9 +1,2 @@
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <?php the_content(); ?>
-        </div>
-    </div>
-</div>
-
+<?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
