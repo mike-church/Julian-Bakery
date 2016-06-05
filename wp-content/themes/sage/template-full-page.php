06e0c3559a6b53b29993f8c11280e9cbe12be7ac
Michael Church <michael@fishinglounge.com>
2016-06-05 11:44:26 -0400

    upgraded Sage

--- wp-content/themes/sage/template-full-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Full Page Template
- */
-?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'full-page'); ?>
-<?php endwhile; ?>
