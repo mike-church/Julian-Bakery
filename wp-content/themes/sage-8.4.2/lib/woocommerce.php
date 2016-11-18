f169ad95b89a93a4feb9ee142addf9c3660f0501
Michael Church <michael@fishinglounge.com>
2016-11-18 09:09:16 -0500

    progress dev

--- wp-content/themes/sage-8.4.2/lib/woocommerce.php
+++ wp-content/themes/sage-8.4.2/lib/woocommerce.php
@@ -18,3 +18,14 @@ function woocommerce_template_loop_rating(){}
 function woocommerce_template_loop_add_to_cart(){}
 function woocommerce_review_display_gravatar(){}
 
+add_filter( 'woocommerce_breadcrumb_defaults', 'jk_change_breadcrumb_home_text' );
+function jk_change_breadcrumb_home_text( $defaults ) {
+    // Change the breadcrumb home text from 'Home' to 'Shop'
+	$defaults['home'] = 'Shop';
+	return $defaults;
+}
+
+add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
+function woo_custom_breadrumb_home_url() {
+    return '/shop';
+}
